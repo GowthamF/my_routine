ba52c78ede1b79054affbc72a38ae45c998ac4c5
Gowtham <gowthamb@pulsesoftware.com>
2021-06-09 10:31:15 +0530

    adding export files

---
+++ my_routine_app/lib/my_routine_app.dart
@@ -0,0 +1,5 @@
+library my_routine_app;
+
+export 'app.dart';
+export 'screens/screens.dart';
+export 'constants/constants.dart';
--- my_routine_bloc/lib/my_routine_bloc.dart
+++ my_routine_bloc/lib/my_routine_bloc.dart
@@ -1,7 +1,1 @@
 library my_routine_bloc;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
